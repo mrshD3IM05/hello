deb69bc6270fe0101ce80254ac9d8e14cd15cccc
John Doe <john@example.com>
2026-03-04 04:49:43 +0000

    Update the lib/hello.sh file

--- lib/hello.sh
+++ lib/hello.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 echo "What's your name"
 read my_name
 
 echo "Hello, $my_name"
+=======
+source lib/greeter.sh
+
+name="$1"
+if [ -z "$name" ]; then
+    name="World"
+fi
+
+Greeter "$name"
+>>>>>>> ac56523 (Update the lib/hello.sh file)
